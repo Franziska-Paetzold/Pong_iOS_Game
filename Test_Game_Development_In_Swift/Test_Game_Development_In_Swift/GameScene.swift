f536bbcfe78ff7114f04ad6d509066105784f9b4
Franziska Paetzold <s0554620@htw-berlin.de>
2018-05-27 22:19:52 +0200

    player paddle is now controllable by users finger

--- Test_Game_Development_In_Swift/Test_Game_Development_In_Swift/GameScene.swift
+++ Test_Game_Development_In_Swift/Test_Game_Development_In_Swift/GameScene.swift
@@ -11,13 +11,14 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var paddle2 = SKSpriteNode()
+    var paddle = SKSpriteNode()
     var token = SKSpriteNode()
     var player = SKSpriteNode()
     
-    //add optional binding
+    //basics inclusive the moving token
+    //TODO: add optional binding
     override func didMove(to view: SKView) {
-        paddle2 = self.childNode(withName: "paddle") as! SKSpriteNode //spritenodes have more functions than nodes --> better for games
+        paddle = self.childNode(withName: "paddle") as! SKSpriteNode //spritenodes have more functions than nodes --> better for games
         token = self.childNode(withName: "token") as! SKSpriteNode
         player = self.childNode(withName: "player") as! SKSpriteNode
         
@@ -32,8 +33,27 @@ class GameScene: SKScene {
         self.physicsBody = border
       
     }
+    
+    //let the player paddle move with the use of a finger
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fingerMovement(touches: touches)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fingerMovement(touches: touches)
+    }
 
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        // enemy paddle following the token a little bit delayed
+        
+        paddle.run(SKAction.moveTo(x: token.position.x , duration: 1.0)) // 1.0 = 1 sek, 0.5 would be way harder to play
+    }
+    
+    func fingerMovement(touches:  Set<UITouch>){
+        for singleTouch in touches{
+            let fingerPosition = singleTouch.location(in: self)
+            
+            player.run(SKAction.moveTo(x: fingerPosition.x , duration: 0.0))
+        }
     }
 }
